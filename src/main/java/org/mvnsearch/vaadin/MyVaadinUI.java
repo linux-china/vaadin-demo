cb633bfefeadbf513bb228e59a656241f0c3e356
linux_china <linux_china@hotmail.com>
2016-10-10 10:33:49 +0800

    minor update

--- src/main/java/org/mvnsearch/vaadin/MyVaadinUI.java
+++ src/main/java/org/mvnsearch/vaadin/MyVaadinUI.java
@@ -8,21 +8,27 @@ import com.vaadin.ui.*;
 import org.springframework.boot.actuate.endpoint.Endpoint;
 import org.springframework.boot.actuate.endpoint.mvc.MvcEndpoint;
 
-@SpringUI(path = "/demo")
-@Title("Vaadin Demo")
+@SpringUI(path = "/my")
+@Title("Vaadin Workspace")
 @Theme("valo")
 public class MyVaadinUI extends UI {
     protected void init(VaadinRequest request) {
         // Create the content root layout for the UI
         VerticalLayout content = new VerticalLayout();
         setContent(content);
+        String nick = (String) getSession().getAttribute("nick");
+        if (nick == null) {
+            nick = "World";
+        }
         // Display the greeting
-        Label label = new Label("Hello World!");
+        Label label = new Label("Hello " + nick + "!");
         content.addComponent(label);
+        TextField textField = new TextField("Email");
+        content.addComponent(textField);
         // Have a clickable button
         content.addComponent(new Button("Push Me!",
                 (Button.ClickListener) e -> {
-                    label.setValue("clicked!!!");
+                    label.setValue(textField.getValue());
                 }));
     }
 
