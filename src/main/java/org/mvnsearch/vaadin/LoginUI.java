df414779c1285de0ef7ee2a80ac398d7167c07e5
linux_china <linux_china@hotmail.com>
2016-10-10 10:33:17 +0800

    login ui

---
+++ src/main/java/org/mvnsearch/vaadin/LoginUI.java
@@ -0,0 +1,43 @@
+package org.mvnsearch.vaadin;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.navigator.Navigator;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.spring.navigator.SpringViewProvider;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * login UI
+ *
+ * @author linux_china
+ */
+@SpringUI(path = "/login")
+@Title("Vaadin Login")
+@Theme("valo")
+public class LoginUI extends UI {
+    @Autowired
+    private SpringViewProvider viewProvider;
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout root = new VerticalLayout();
+        root.setSizeFull();
+        root.setMargin(true);
+        root.setSpacing(true);
+        setContent(root);
+
+        final Panel viewContainer = new Panel();
+        viewContainer.setSizeFull();
+        root.addComponent(viewContainer);
+        root.setExpandRatio(viewContainer, 1.0f);
+
+        Navigator navigator = new Navigator(this, viewContainer);
+        navigator.addProvider(viewProvider);
+        getUI().getNavigator().navigateTo("loginview");
+    }
+}
