37c19676c5a94e4803453304f69fde3cbc0ef51d
linux_china <linux_china@hotmail.com>
2017-02-17 17:30:08 +0800

    admin3 vaadin ui

---
+++ src/main/java/org/mvnsearch/vaadin/Admin3VaadinUI.java
@@ -0,0 +1,30 @@
+package org.mvnsearch.vaadin;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.spring.annotation.SpringUI;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@SpringUI(path = "/admin3")
+@Title("Vaadin Admin")
+@Theme("valo")
+public class Admin3VaadinUI extends UI {
+    protected void init(VaadinRequest request) {
+        // Create the content root layout for the UI
+        VerticalLayout content = new VerticalLayout();
+        setContent(content);
+        // Display the greeting
+        Label label = new Label("Admin3!");
+        content.addComponent(label);
+        // Have a clickable button
+        content.addComponent(new Button("Push Me!",
+                (Button.ClickListener) e -> {
+                    label.setValue("clicked!!!");
+                }));
+    }
+
+}
