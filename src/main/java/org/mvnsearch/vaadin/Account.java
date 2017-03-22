4fbb3b9451a81e9bde94d261d3e35c03bcdd182b
linux_china <linux_china@hotmail.com>
2017-03-22 20:08:59 +0800

    account

---
+++ src/main/java/org/mvnsearch/vaadin/Account.java
@@ -0,0 +1,27 @@
+package org.mvnsearch.vaadin;
+
+/**
+ * account
+ *
+ * @author linux_china
+ */
+public class Account {
+    private String nick;
+    private String password;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
