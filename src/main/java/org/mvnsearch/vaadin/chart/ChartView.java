25bfe9c2ecadb47e1c894655ff4341b4f5521e7e
linux_china <linux_china@hotmail.com>
2017-03-23 16:49:51 +0800

    add pie chart

--- src/main/java/org/mvnsearch/vaadin/chart/ChartView.java
+++ src/main/java/org/mvnsearch/vaadin/chart/ChartView.java
@@ -1,10 +1,13 @@
 package org.mvnsearch.vaadin.chart;
 
 import com.vaadin.addon.charts.Chart;
-import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.PointClickEvent;
+import com.vaadin.addon.charts.PointClickListener;
+import com.vaadin.addon.charts.model.*;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.VerticalLayout;
 
 import javax.annotation.PostConstruct;
@@ -20,10 +23,50 @@ public class ChartView extends VerticalLayout implements View {
 
     @PostConstruct
     public void init() {
-        Chart chart = new Chart(ChartType.COLUMN);
-        chart.setWidth("400px"); // 100% by default
-        chart.setHeight("300px"); // 400px by default
+        Chart chart = createChart();
         addComponent(chart);
+    }
+
+    private Chart createChart() {
+        Chart chart = new Chart(ChartType.PIE);
+
+        Configuration conf = chart.getConfiguration();
+
+        conf.setTitle("Browser market shares at a specific website, 2010");
+
+        PlotOptionsPie plotOptions = new PlotOptionsPie();
+        plotOptions.setCursor(Cursor.POINTER);
+        DataLabels dataLabels = new DataLabels();
+        dataLabels.setEnabled(true);
+        dataLabels
+                .setFormatter("'<b>'+ this.point.name +'</b>: '+ this.percentage +' %'");
+        plotOptions.setDataLabels(dataLabels);
+        conf.setPlotOptions(plotOptions);
+
+        final DataSeries series = new DataSeries();
+        series.add(new DataSeriesItem("Firefox", 45.0));
+        series.add(new DataSeriesItem("IE", 26.8));
+        DataSeriesItem chrome = new DataSeriesItem("Chrome", 12.8);
+        chrome.setSliced(true);
+        chrome.setSelected(true);
+        series.add(chrome);
+        series.add(new DataSeriesItem("Safari", 8.5));
+        series.add(new DataSeriesItem("Opera", 6.2));
+        series.add(new DataSeriesItem("Others", 0.7));
+        conf.setSeries(series);
+
+        chart.addPointClickListener(new PointClickListener() {
+
+            @Override
+            public void onClick(PointClickEvent event) {
+                Notification.show("Click: "
+                        + series.get(event.getPointIndex()).getName());
+            }
+        });
+
+        chart.drawChart(conf);
+
+        return chart;
     }
 
     @Override
